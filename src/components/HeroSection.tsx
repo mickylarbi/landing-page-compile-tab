6c7fc018fb01f36835197ea0d9551e361c276f66
Michael Larbi <mickylarbi28@gmail.com>
2026-01-21 06:00:18 +0000

    build hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,48 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+import GradientButton from "./GradientButton";
+import OutlineButton from "./OutlineButton";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl, sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools{"  "}
+        <span className="bg-linear-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          for developers
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality.
+      </p>
+      <div className="flex justify-center my-10 space-x-4">
+        <GradientButton text="Start for free" />
+        <OutlineButton text="Documentation" />
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
